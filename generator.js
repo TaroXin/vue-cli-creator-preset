37fe3ee3faa23cd7e264ac421936f1cf243216c3
Taro <15029352778@163.com>
2018-08-15 22:30:19 +0800

    initial

---
+++ generator.js
@@ -0,0 +1,14 @@
+module.exports = (api, options, rootOptions) => {
+    // 修改 `package.json` 里的字段
+    api.extendPackage({
+      scripts: {
+        test: 'vue-cli-service test',
+        'build:android': 'echo "Build"'
+      }
+    })
+  
+    // 复制并用 ejs 渲染 `./template` 内所有的文件
+    if (options.foo) {
+      // 有条件地生成文件
+    }
+  }
